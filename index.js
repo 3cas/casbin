ec3be7e0070de0bfd06096e6a571e4d44b4ea53c
CAS <78571139+cas-14@users.noreply.github.com>
2022-01-03 13:43:26 -0500

    fix queery

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ const firebaseConfig = {
   // For example, https://your-database-123.europe-west1.firebasedatabase.app
   databaseURL: "https://cas-bin-a69e7-default-rtdb.firebaseio.com/",
   storageBucket: "bucket.appspot.com"
-};
+}
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 
 // Get a reference to the database service
-const db = getDatabase();
+const db = getDatabase()
 
 function createComment(pid, text) {
     var now = new Date()
     set(push(ref(db, "comments/" + pid)), {
         content: text,
         timestamp: Date.parse(now),
-    });
-    return "left a comment";
+    })
+    return "left a comment"
 }
 
 function createPost(title, text, publicize, allowcom) {
@@ -35,17 +35,18 @@ function createPost(title, text, publicize, allowcom) {
         timestamp: Date.parse(now),
         public: publicize,
         comments: allowcom,
-    });
-    return "post created";
+    })
+    return "post created"
 }
-  
+
 function addQuick() {
-    return createPost("post", document.getElementById("postContentBox").value, true, true);
+    return createPost("post", document.getElementById("postContentBox").value, true, true)
 }
 
 function getPosts() {
-    var latestPosts = ref(db, "posts/").orderByChild("timestamp").limitToLast(100).get();
-    console.log(latestPosts);
+    var latestPosts = query(ref(db, "posts/"), orderByChild("timestamp"), limitToLast(100))
+    latestPosts = latestPosts.get()
+    console.log(latestPosts)
 }
 
 window.createComment = createComment
