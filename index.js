b38200909c711eee15832945248d9cdf32ffabca
CAS <78571139+cas-14@users.noreply.github.com>
2022-04-27 12:27:04 -0400

    f

--- index.js
+++ index.js
@@ -44,8 +44,7 @@ function addQuick() {
 }
 
 function getPosts() {
-    const ref = db.ref('posts');
-    ref.orderByChild('timestamp').limitToLast(10).on('child_added', (snapshot) => {
+    db.ref('posts').orderByChild('timestamp').limitToLast(10).on('child_added', (snapshot) => {
         value = snapshot.val()
         console.log("post id: "+snapshot.key+"\npost title: "+value.title+"\ncontent: "+value.content);
     });
